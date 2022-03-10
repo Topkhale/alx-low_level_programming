a1d071f32a6d5dfcfe60d1e52ea6b4f0e556d2e9
Topkhale <topkhale@gmail.com>
2022-03-10 05:30:59 -0800

    kj hg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -13,4 +12,4 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-}
+iii}
