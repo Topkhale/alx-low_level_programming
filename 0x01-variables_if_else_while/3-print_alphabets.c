b442f94f34f33353eb18abf53d0560575b5a39b5
Topkhale <topkhale@gmail.com>
2022-03-11 07:53:03 -0800

    kj hg

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-
-
-/**
-
- * main - Prints the alphabet.
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
-{
-
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-	int i;
-
-
-
-	for (i = 0; i < 52; i++)
-
-	{
-
-		putchar(alp[i]);
-
-	}
-
-	putchar('\n');
-
-	return (0);
-
-}
-=======
 #include <stdio.h>
 
 /**
@@ -56,4 +17,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
->>>>>>> 3b11e22d51675b6fab6045e14ffcbf6704bc42af
